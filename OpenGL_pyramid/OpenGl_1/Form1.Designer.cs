1a4206553bc4b86ee9318530b28d230affd4aefe
Melisa Akyol <46058934+melisakyoll@users.noreply.github.com>
2020-12-26 18:43:36 +0300

    OpenGL added.

---
+++ OpenGL_pyramid/OpenGl_1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace OpenGl_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenGlControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.buttonDondur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenGlControl
+            // 
+            this.OpenGlControl.AccumBits = ((byte)(0));
+            this.OpenGlControl.AutoCheckErrors = false;
+            this.OpenGlControl.AutoFinish = false;
+            this.OpenGlControl.AutoMakeCurrent = true;
+            this.OpenGlControl.AutoSwapBuffers = true;
+            this.OpenGlControl.BackColor = System.Drawing.Color.Black;
+            this.OpenGlControl.ColorBits = ((byte)(32));
+            this.OpenGlControl.DepthBits = ((byte)(16));
+            this.OpenGlControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.OpenGlControl.Location = new System.Drawing.Point(0, 0);
+            this.OpenGlControl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.OpenGlControl.Name = "OpenGlControl";
+            this.OpenGlControl.Size = new System.Drawing.Size(613, 519);
+            this.OpenGlControl.StencilBits = ((byte)(0));
+            this.OpenGlControl.TabIndex = 0;
+            this.OpenGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.MyPaint);
+            this.OpenGlControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeyDown);
+            // 
+            // buttonDondur
+            // 
+            this.buttonDondur.BackColor = System.Drawing.Color.Violet;
+            this.buttonDondur.Location = new System.Drawing.Point(644, 15);
+            this.buttonDondur.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonDondur.Name = "buttonDondur";
+            this.buttonDondur.Size = new System.Drawing.Size(100, 50);
+            this.buttonDondur.TabIndex = 1;
+            this.buttonDondur.Text = "Döndür";
+            this.buttonDondur.UseVisualStyleBackColor = false;
+            this.buttonDondur.Click += new System.EventHandler(this.buttonDondur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 519);
+            this.Controls.Add(this.buttonDondur);
+            this.Controls.Add(this.OpenGlControl);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "18-701-038";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyKeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlControl;
+        private System.Windows.Forms.Button buttonDondur;
+    }
+}
+
---
+++ OpenGL_pyramid/OpenGl_1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+
+namespace OpenGl_1
+{
+    public partial class Form1 : Form
+    {
+        float yatay = 0; 
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity(); 
+            Gl.glOrtho(2.0, 2.0 , 2.0,2.0,-2.0,2.0); 
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // M harfine basınca +5 derece döndürüyor. A harifne basında -5 derece döndürüyor. Döndürmesi için siyah ekrana basın. 
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+            
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glShadeModel(Gl.GL_SMOOTH);
+            Gl.glPushMatrix();
+            Gl.glTranslatef(0.0f, 0.0f, 0.0f); 
+            Gl.glRotatef(yatay, 0, 1, 0);
+            Gl.glTranslatef(-0.0f, -0.0f, 0.0f);
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+
+            
+            //ön
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.5f, 0.0f);
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-0.5f, -0.5f, 0.5f);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(0.5f, -0.5f, 0.5f);
+
+
+            //sağ
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.5f, 0.0f);
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.5f, -0.5f, 0.5f);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(0.5f, -0.5f, -0.5f);
+
+
+            //arka
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.5f, 0.0f);
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.5f, -0.5f, -0.5f);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(-0.5f, -0.5f, -0.5f);
+
+
+            //sol
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.5f, 0.0f);
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-0.5f, -0.5f, -0.5f);
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(-0.5f, -0.5f, 0.5f);
+
+            Gl.glEnd();
+            Gl.glPopMatrix();
+        }
+
+        private void buttonDondur_Click(object sender, EventArgs e)
+        {          
+            yatay = (yatay+10) % 360; 
+            OpenGlControl.Refresh(); 
+        }
+
+        private void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+                yatay = (yatay + 5) % 360;
+            if (e.KeyCode == Keys.A)
+                yatay = (yatay - 5) % 360;
+
+            OpenGlControl.Refresh();
+        }
+    }
+}
